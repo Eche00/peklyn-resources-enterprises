e1b02aa512328067d65acda81aa9604aa5450cef
Eche00 <echeeze956@gmail.com>
2025-02-04 02:06:42 +0100

    footer 70%

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,56 @@
+import { Facebook, Instagram, WhatsApp } from "@mui/icons-material";
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className=" w-full bg-black  py-[50px]">
+      {/* container  */}
+      <footer className=" flex flex-col w-[95%] mx-auto text-white">
+        {/* top section  */}
+        <div className=" flex items-center justify-between flex-col sm:flex-row gap-[20px]">
+          <h2 className=" sm:text-[30px] text-[18px] font-extrabold">
+            Lorem ipsum dolor sit amet.
+          </h2>
+          <section className=" bg-white rounded-full p-[5px]">
+            <input
+              type="text"
+              className=" outline-none text-black py-2 px-[10px]"
+            />{" "}
+            <button className=" bg-blue-600 py-3 px-6 rounded-full">
+              Subscribe
+            </button>
+          </section>
+        </div>
+
+        {/* bottom section  */}
+        <div className=" flex py-[20px] sm:flex-row flex-col">
+          <section className="flex flex-col flex-1">
+            <section className=" w-[150px] bg-white h-[40px]">
+              <img src="" alt="" />
+            </section>
+            <p className="sm:w-[400px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur accusantium quis magnam harum quasi, incidunt culpa,
+              cupiditate sed dolorum deserunt dolore doloribus minus sequi
+              necessitatibus nihil quam eum voluptatem ullam.
+            </p>
+            <article className=" flex gap-[10px] list-disc list-inside py-[10px]">
+              <span className=" bg-blue-600 p-[7px] rounded-full">
+                <Facebook />
+              </span>
+              <span className=" bg-blue-600 p-[7px] rounded-full">
+                <Instagram />
+              </span>
+              <span className=" bg-blue-600 p-[7px] rounded-full">
+                <WhatsApp />
+              </span>
+            </article>
+          </section>
+          <section className="flex flex-col flex-1">2</section>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Hero from "../Home/Hero";
+import Service from "../Home/Service";
+import About from "../Home/AboutUs";
+import Support from "../Home/Support";
+import Featured from "../Home/Featured";
+import Together from "../Home/Together";
+import Reviews from "../Home/Reviews";
 
 function Home() {
-  return <div className=" text-black">Home</div>;
+  return (
+    <div className="">
+      <div>
+        <Hero />
+        <Service />
+        <About />
+        <Support />
+        <Featured />
+        <Reviews />
+        <Together />
+        <Hero />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
